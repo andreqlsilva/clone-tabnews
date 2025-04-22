abad3ee9fbe58f91f0f258a0bd30b33e794121ae
andreqlsilva <andreqlsilva@gmail.com>
2025-04-22 18:00:41 +0000

    Acrescenta dados no endpoint /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,18 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Não existe almoço grátis" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const usedConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity;",
+  );
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: postgresVersion.rows[0].server_version,
+    max_connections: parseInt(maxConnections.rows[0].max_connections),
+    used_connections: usedConnections.rows[0].count,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,28 @@
-test("GET to /api/v1/status should return 200", async () => {
+test("GET no /api/v1/status deve retornar o estado", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+
+  // Was the fetch succesful?
   expect(response.status).toBe(200);
+
+  // Are all parameters defined?
+  const responseBody = await response.json();
+  //console.log(responseBody);
+
+  // Is updated_at a date?
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  // Does updated_at match the ISO date format?
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  // Is postgres_version a valid version number?
+  const versionRegex = /([1-9](\d*))+/;
+  expect(versionRegex.test(responseBody.postgres_version)).toBe(true);
+
+  // Is max_connections a positive integer?
+  expect(responseBody.max_connections).not.toBe(NaN);
+  expect(responseBody.max_connections > 0).toBe(true);
+
+  // Is used_connections a non-negative integer that's less than max_connections?
+  expect(responseBody.used_connections).not.toBe(NaN);
+  expect(responseBody.used_connections).toBe(1);
 });
