6f3380fb7e412456af674716e4636e2110f46aa4
andreqlsilva <andreqlsilva@gmail.com>
2025-04-25 01:13:32 +0000

    Corrige formato do endpoint /api/v1/status e aprimora tratamento de erros no database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -12,9 +12,13 @@ async function status(request, response) {
   });
   response.status(200).json({
     updated_at: updatedAt,
-    postgres_version: postgresVersion.rows[0].server_version,
-    max_connections: parseInt(maxConnections.rows[0].max_connections),
-    used_connections: usedConnections.rows[0].count,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        used_connections: usedConnections.rows[0].count,
+      },
+    },
   });
 }
 
