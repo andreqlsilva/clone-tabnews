ba8143e1125b46cd7bc655350a9bc8bcd7950566
tecnot <code@tecnot.com.br>
2025-05-25 00:53:57 -0400

    Provisoriamente admite requisições com DELETE para limpar o banco

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -21,6 +21,7 @@ export default async function migrations(request, response) {
   }
 
   else if (request.method === 'POST') {
+    await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
     const migratedMigrations = await migrationRunner({
       ...defMigOpt,
       dryRun: false,
@@ -30,6 +31,12 @@ export default async function migrations(request, response) {
       return response.status(201).json(migratedMigrations);
     }
     else return response.status(200).json(migratedMigrations);
+  }
+  else if (request.method === 'DELETE') {
+    await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
+    const pendingMigrations = await migrationRunner(defMigOpt);
+    await dbClient.end();
+    return response.status(201).json(pendingMigrations);
   }
 
   await dbClient.end();
