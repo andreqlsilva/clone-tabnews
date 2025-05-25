694139f1699fc52538e27c4596131e8c0a2e4555
tecnot <code@tecnot.com.br>
2025-05-25 00:38:32 -0400

    Induz erro de múltiplas conexões para requisições GET, PUT ou DELETE

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -15,7 +15,7 @@ export default async function migrations(request, response) {
 
   if (request.method === 'GET') {
     const pendingMigrations = await migrationRunner(defMigOpt);
-    await dbClient.end();
+//    await dbClient.end();
 
     return response.status(200).json(pendingMigrations);
   }
@@ -32,7 +32,7 @@ export default async function migrations(request, response) {
     else return response.status(200).json(migratedMigrations);
   }
 
-  await dbClient.end();
+//  await dbClient.end();
   return response.status(405).end();
 }
 
