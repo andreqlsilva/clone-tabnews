0c124bc46da2bd3075fbcf8858cb92352c8d1ce7
Andre Silva <andreqlsilva@gmail.com>
2025-06-09 00:59:24 -0400

    feat: add `ServiceError`

--- infra/controllers.js
+++ infra/controllers.js
@@ -16,16 +16,16 @@ function configureRouter(handlers) {
 
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
-  console.log("\nErro por uso de método não permitido.");
-  //    console.log(publicErrorObject.stack);
+  console.error(publicErrorObject);
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
 function onErrorHandler(error, request, response) {
   const publicErrorObject = new InternalServerError({
+    statusCode: error.statusCode,
     cause: error,
   });
   console.log("\nErro dentro do catch do next-connect:");
-  //   console.log(publicErrorObject.stack);
+  console.error(publicErrorObject);
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
